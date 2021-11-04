c2f609f5729b5f68b168fa3689cd52d97068fbff
Tafiqul <demianviotto@gmail.com>
2021-11-04 19:04:07 -0400

    solved task 7

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ function addReview(array, name, rating, feedback){
   array.push(obj);
   return array;
 }
+// console.log(reviews, 'Daniela', 5, 'Beautiful atmosphere and wonderful vegan options!')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
@@ -136,7 +137,7 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  return `${array[array.length -1].name} gave the restaurant a ${array[array.length -1].rating} star review, and their feedback was: ${array[array.length -1].feedback} `;
+  return `${array[array.length -1].name} gave the restaurant a ${array[array.length -1].rating} star review, and their feedback was: ${array[array.length -1].feedback}`;
 } 
 
 console.log(getLastReview(reviews));
